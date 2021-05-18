01e42921da78d626ae453cf8508a9a945da28089
rollexxx23 <arinchatterjee00@gmail.com>
2021-05-18 20:32:34 +0530

    added smart contract , TODO : connect with react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css' ;
 import FirstPage from "./Login/FirstPage"
 import Login from "./Login/Login"
 import EnterOTP from "./Login/EnterOTP"
@@ -8,10 +11,30 @@ import { Switch,Route } from "react-router-dom";
 import Error from "./Error";
 import ThankYou from "./Screens/ThankYou"
 
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
 
-function App() {
-  return (
-    <div className="App">
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const ethereum = window.ethereum;
+    const enabledWeb3 = await ethereum.enable();
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] }) ;
+    console.log(this.state.account) ;
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '' }
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
       <Switch>
       <Route path='/' exact component={FirstPage} />
       <Route path='/login' exact component={Login} />
@@ -21,8 +44,11 @@ function App() {
       <Route path='/thank' exact component={ThankYou} />
       <Route component={Error} />
       </Switch>
+
+      <p>Your account: {this.state.account}</p>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
